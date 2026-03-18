fe96990d00b6d78bab6d9db59b9c04f0ed3ffc85
Luciano Lucena <webluc@gmail.com>
2026-03-18 14:08:59 -0300

    Update date handling to use pd.Timestamp

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -42,9 +42,9 @@ st.sidebar.markdown( '### Selecione uma data limite' )
 
 date_slider = st.sidebar.slider(
     'Até que valor ?',
-    value=pd.datetime( 2023, 3, 28 ),
-    min_value = pd.datetime( 2022, 2, 11 ),
-    max_value = pd.datetime( 2022, 4, 6 ),
+    value=pd.Timestamp( 2023, 3, 28 ),
+    min_value = pd.Timestamp( 2022, 2, 11 ),
+    max_value = pd.Timestamp( 2022, 4, 6 ),
     format = 'DD-MM-YYYY' )
 
 st.sidebar.markdown( """---""" )
