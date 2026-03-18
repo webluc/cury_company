f54110b670800a4375773052c6f41d82613c3997
Luciano Lucena <webluc@gmail.com>
2026-03-18 15:06:23 -0300

    Fix date formatting in date slider

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -38,9 +38,9 @@ st.sidebar.markdown( '### Selecione uma data limite' )
 
 date_slider = st.sidebar.slider(
     'Até que valor ?',
-    value=pd.datetime.date(2023,03,28),
-    min_value = pd.datetime.date(2022,02,11),
-    max_value = pd.datetime.date(2022,04,06),
+    value=pd.datetime.date(2023,3,28),
+    min_value = pd.datetime.date(2022,2,11),
+    max_value = pd.datetime.date(2022,4,6),
     format = 'DD-MM-YYYY' )
 
 st.sidebar.markdown( """---""" )
