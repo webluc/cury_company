d3d6a36249ee2a64b9be2f220fd8ee3ab2bfd5b2
Luciano Lucena <webluc@gmail.com>
2026-03-18 15:58:02 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -61,7 +61,7 @@ st.sidebar.markdown( '#### Powered ComunidadeDS'  )
 # ----------------------------------------
 
 # Filtro de Data
-linhas_selecionadas = df1[df'['Order_Date'] <= pd.to_datetime(date_slider)]
+linhas_selecionadas = df'['Order_Date'] < pd.to_datetime(date_slider)
 df1 = df1.loc[ linhas_selecionadas, : ]
 
 # Filtro de Traffic 
