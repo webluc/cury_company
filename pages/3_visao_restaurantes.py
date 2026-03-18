af228a93953b1658b55e7b390e73b7ed2416d7ef
Luciano Lucena <webluc@gmail.com>
2026-03-18 14:24:54 -0300

    Fix date format in date slider component

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -43,9 +43,9 @@ st.sidebar.markdown( '### Selecione uma data limite' )
 
 date_slider = st.sidebar.slider(
     'Até que valor ?',
-    value=pd.Timestamp( 2023, 3, 28 ),
-    min_value = pd.Timestamp( 2022, 2, 11 ),
-    max_value = pd.Timestamp( 2022, 4, 6 ),
+    value=pd.Timestamp( '2023-03-28' ),
+    min_value = pd.Timestamp( '2022-02-11' ),
+    max_value = pd.Timestamp( '2022-04-06' ),
     format = 'DD-MM-YYYY' )
 
 st.sidebar.markdown( """---""" )
